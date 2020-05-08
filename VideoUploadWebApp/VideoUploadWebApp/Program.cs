8a29468ec03b0ceeaca89fdaa5df462429d52935
Gavin Lefebvre <glefebvr@epic47282.epic.com>
2020-05-08 07:34:03 -0500

    more filesize

--- VideoUploadWebApp/VideoUploadWebApp/Program.cs
+++ VideoUploadWebApp/VideoUploadWebApp/Program.cs
@@ -13,6 +13,11 @@ namespace VideoUploadWebApp
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureKestrel((context, options) =>
+                {
+                    // Handle requests up to 200 MB
+                    options.Limits.MaxRequestBodySize = 209715200;
+                })
                 .Build();
     }
 }
