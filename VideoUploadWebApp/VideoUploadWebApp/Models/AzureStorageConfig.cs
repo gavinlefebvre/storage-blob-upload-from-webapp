a58d17bd69d0a42f6e90dd6a2460822c0a734ae3
Gavin Lefebvre <glefebvr@epic47282.epic.com>
2020-05-07 23:13:55 -0500

    pivot to video

---
+++ VideoUploadWebApp/VideoUploadWebApp/Models/AzureStorageConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace VideoUploadWebApp.Models
+{
+    public class AzureStorageConfig
+    {
+        public string AccountName { get; set; }
+        public string AccountKey { get; set; }
+        public string VideoContainer { get; set; }
+        public string ThumbnailContainer { get; set; }
+    }
+}
